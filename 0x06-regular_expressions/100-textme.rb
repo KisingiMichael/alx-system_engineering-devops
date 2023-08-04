16b9dcd32c08514706aca94c63c82c29cdbfa4b6
KisingiMichael <kisingimichael12@gmail.com>
2023-08-03 19:16:34 -0700

    Apply fix for second deadline

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -3,4 +3,4 @@
 #The sender phone number or name (including country code if present)
 #The receiver phone number or name (including country code if present)
 #The flags that were used
-puts ARGV[0].scan(/(?<=from: |to: |flags:)(.+?)(?=\])/).join(",")
+puts ARGV[0].scan(/(?<=from:|to:|flags:).+?(?=\])/).join(',')
