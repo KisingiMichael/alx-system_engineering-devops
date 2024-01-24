133724259a6f542fe13905c92dbc5abc0a0b2b2a
KisingiMichael <kisingimichael12@gmail.com>
2024-01-24 17:59:58 +0300

    modify puppet file

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,17 +1,20 @@
 #automating works using puppet
 
-package { 'nginx':
-  ensure => installed,
+exec { 'update system':
+        command => '/usr/bin/apt-get update',
 }
-file_line { 'install':
-  ensure => 'present',
-  path   => 'etc/nginx/sites-enabled/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^redirect_me https://www.github.com/KisingiMichael permanent;',
+
+package { 'nginx':
+          ensure  => installed,
+          require => Exec['update system']
+}
+exec { 'redirect_me':
+       command => 'sed -i "241\ rewrite ^redirect_me https://www.github.com/KisingiMichael permanent;" /etc/nginx/sites-enabled/default',
+       provider => 'shell'
 }
 
 file { '/var/www/html/index.html':
-  content => 'Hello World!',
+       content => 'Hello World!'
 }
 
 service { 'nginx':
