b32627aa809300405f27c2b1b43a979a9c9a7f23
KisingiMichael <kisingimichael12@gmail.com>
2024-01-27 18:30:32 +0300

    fix pylint issue in 1-top_ten file

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,11 +9,10 @@ headers = {"User-Agent": "MyCustomUserAgent/1.0"}
 
 def top_ten(subreddit):
     """method doc"""
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    response = requests.get(url, allow_redirects=False, headers=headers)
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    response = requests.get(url, headers)
     if response.status_code == 200:
         data = response.json()
         for post in data["data"]["children"]:
             print(post["data"]["title"])
-    else:
         print("None")
