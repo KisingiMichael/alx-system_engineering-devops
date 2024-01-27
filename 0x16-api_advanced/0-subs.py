4905b74994f1922a1de5328cc079967995ad61e3
KisingiMichael <kisingimichael12@gmail.com>
2024-01-27 16:02:39 +0300

    Return number of subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Module documentation """
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ Function implementation """
+    url = 'https://www.reddit.com'
+    header = {
+        'Accept': 'application/json',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+    }
+    response = requests.get('{}/r/{}/about/.json'.format(url, subreddit),
+                           headers=header, allow_redirects=False)
+    if response.status_code == 200:
+        return response.json()['data']['subscribers']
+    return 0
