8f80702729272d2fa8ba902fcbefae9354ee4eb1
KisingiMichael <kisingimichael12@gmail.com>
2024-01-27 18:26:36 +0300

    fix pylint

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,7 +12,7 @@ def number_of_subscribers(subreddit):
     Function Docs
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    response = requests.get(url, allow_redirects=False, headers=headers)
+    response = requests.get(url, headers)
     if response.status_code == 200:
         data = response.json()
         return data["data"]["subscribers"]
