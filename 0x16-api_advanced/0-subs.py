69872c012e3ff13f8e1564e627de76d27627293e
KisingiMichael <kisingimichael12@gmail.com>
2024-01-27 16:14:11 +0300

    more fixes

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """
-Module documentation
+Module Docs
 """
 import requests
 
 
 def number_of_subscribers(subreddit):
     """
-    Function implementation
+    Function Docs
     """
     url = 'https://www.reddit.com'
     header = {
@@ -15,7 +15,9 @@ def number_of_subscribers(subreddit):
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
     }
     response = requests.get('{}/r/{}/about/.json'.format(url, subreddit),
-                            headers=header, allow_redirects=False)
+                            headers=header,
+                            allow_redirects=False
+                            )
     if response.status_code == 200:
         return response.json()['data']['subscribers']
     return 0
