3356deca4a8d01985385afff1b5fff97ed3bc194
KisingiMichael <kisingimichael12@gmail.com>
2024-08-28 10:25:08 +0300

    perform modification

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,6 +8,7 @@ for a given subreddit.
 If an invalid subreddit is given, the function should return 0.
 """
 
+
 def number_of_subscribers(subreddit):
     # Define the URL for the subreddit's JSON data
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
